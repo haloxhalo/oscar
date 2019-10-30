0466472827dfdae6e91611f03b58a1ea70ba884b
Eman Calso <egcalso@gmail.com>
2019-10-31 04:07:37 +0800

    black

--- oscar/scans/urls.py
+++ oscar/scans/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 
-from oscar.scans.views import (
-    add_scan_view,
-)
+from oscar.scans.views import add_scan_view
 
 app_name = "scans"
-urlpatterns = [
-    path("/", view=add_scan_view, name="add_scan"),
-]
+urlpatterns = [path("/", view=add_scan_view, name="add_scan")]
