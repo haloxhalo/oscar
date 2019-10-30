44561b967b0ee721623c3536cfd00dd2dfb134d5
ErwinPacheco <erwinabelardo.g.pacheco@gmail.com>
2019-10-30 20:59:51 +0000

    Remove extra slash

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="XF32xLRPKk92ToE9s2yDovvXObtsjHt3AGXQBpur6UmuK3lTscWEpMBykFwqEoy6",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "128.199.237.86:8001"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "128.199.237.86"]
 
 # CACHES
 # ------------------------------------------------------------------------------
--- oscar/scans/urls.py
+++ oscar/scans/urls.py
@@ -3,4 +3,4 @@ from django.urls import path
 from oscar.scans.views import add_scan_view
 
 app_name = "scans"
-urlpatterns = [path("/", view=add_scan_view, name="add_scan")]
+urlpatterns = [path("", view=add_scan_view, name="add_scan")]
