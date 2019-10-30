092072e223080f507982b6071867b64759be8be6
ErwinPacheco <erwinabelardo.g.pacheco@gmail.com>
2019-10-30 22:18:14 +0000

    Add products views & scans views & template

--- oscar/products/views.py
+++ oscar/products/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from .models import Company, SKU
 
-# Create your views here.
+
+class DetailCompanyView(DetailView):
+    model = Company
+    fields = ['data']
+
+
+class ListCompanyView(ListView):
+    model = Company
+    fields = ['data']
+
+
+class DetailSKUView(DetailView):
+    model = SKU
+    fields = ['data','company']
+
+
+class ListSKUView(ListView):
+    model = SKU
+    fields = ['data','company']
+
+
+detail_company_view = DetailCompanyView.as_view()
+list_company_view = ListCompanyView.as_view()
+detail_sku_view = DetailSKUView.as_view()
+list_sku_view = ListSKUView.as_view()
+
--- oscar/scans/views.py
+++ oscar/scans/views.py
@@ -5,6 +5,6 @@ from .models import Scan
 
 class AddScanView(CreateView):
     model = Scan
-
+    fields = ['data']
 
 add_scan_view = AddScanView.as_view()
